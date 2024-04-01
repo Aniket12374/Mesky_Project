16cecb41d94800a70a91bbadce7896af7b6c3554
manan-sharma01 <manan.sharma@mesky.com>
2024-04-01 21:40:09 +0530

    added filter to both subscription and history table

--- src/components/History/ListingPage.jsx
+++ src/components/History/ListingPage.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useQuery } from "react-query";
 import DataTable from "../Common/DataTable/DataTable";
 import { previousOrders } from "../../services/subscriptionOrders/subscriptionService";
@@ -9,9 +10,26 @@ const ListingPage = () => {
     previousOrders
   );
   const navigate = useNavigate();
+
   if (isError) {
     return navigate("/login");
   }
+
+  const uniqueSocietyNames = Array.from(
+    new Set(data?.data?.data.map((listingData) => listingData?.society?.name))
+  );
+  const uniqueAgentNames = Array.from(
+    new Set(
+      data?.data?.data.flatMap((listingData) =>
+        listingData?.rider?.map((rider) => rider.full_name)
+      )
+    )
+  );
+  const uniqueStatuses = Array.from(
+    new Set(
+      data?.data?.data.map((listingData) => listingData?.status?.del_status)
+    )
+  );
 
   let historyData = [];
   data?.data?.data.map((listingData) => {
@@ -21,12 +39,12 @@ const ListingPage = () => {
       customer_name: listingData?.order?.full_name,
       society_name: listingData?.society?.name,
       delivery: listingData?.order?.line_1 + " " + listingData?.order?.line_2,
-      align: "center",
+      // align: "center",
       agent_name: listingData?.rider?.map((rider, key) => {
         let comma = ridersCount - 1 !== key ? ", " : "";
         return rider.full_name + comma;
       }),
-      status: listingData?.status?.del_status,
+      status: listingData?.status?.status || "In Transit", // Defaulting to "In Transit" if no status available
     });
   });
 
@@ -35,39 +53,48 @@ const ListingPage = () => {
       title: "ORDER ID",
       dataIndex: "order_id",
       key: "order_id",
-      // align: "center",
       width: 100,
     },
     {
       title: "CUSTOMER NAME",
       dataIndex: "customer_name",
-      // align: "center",
       key: "customer_name",
     },
     {
       title: "SOCIETY NAME",
       dataIndex: "society_name",
-      // align: "center",
       key: "society_name",
+      filters: uniqueSocietyNames.map((societyName) => ({
+        text: societyName,
+        value: societyName,
+      })),
+      onFilter: (value, record) => record.society_name === value,
     },
     {
       title: "DELIVERY ADDRESS",
       dataIndex: "delivery",
-      // align: "center",
       key: "delivery",
     },
     {
       title: "AGENT NAME",
       dataIndex: "agent_name",
       key: "agent_name",
-      // align: "center",
+      filters: uniqueAgentNames.map((agentName) => ({
+        text: agentName,
+        value: agentName,
+      })),
+      onFilter: (value, record) => record.agent_name.includes(value),
       width: 200,
     },
     {
       title: "STATUS",
       dataIndex: "status",
       key: "status",
-      // align: "center",
+      filters: uniqueStatuses.map((status) => ({
+        text: status,
+        value: status,
+      })),
+      onFilter: (value, record) => record.status === value,
     },
   ];
 

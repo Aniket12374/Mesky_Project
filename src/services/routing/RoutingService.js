8264c41df414339e97b41004155b3aa218c1d2db
manan-sharma01 <manan.sharma@mesky.com>
2024-04-16 19:53:02 +0530

    routing service

--- src/services/routing/RoutingService.js
+++ src/services/routing/RoutingService.js
@@ -1,8 +1,8 @@
 import { httpVendor } from "../api-client";
 
-// export const assignAgent = (data) => {
-//   return httpVendor.post("/api/delivery/portal/area_mapping", data);
-// };
+export const riderInfo = (data) => {
+  return httpVendor.post("/api/delivery/portal/ranking", data);
+};
 
 export const routingStats = () => {
   return httpVendor.get("/api/delivery/portal/ranking?rider_id=2");

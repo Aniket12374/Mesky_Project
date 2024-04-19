2a34289ac4e1bb2fc2a18d4135ebeb030ccb1da6
aneela <aneela.chalamcharla@mesky.com>
2024-04-19 13:42:49 +0530

    area map reerted

--- src/components/AreaMapping/AreaMap.jsx
+++ src/components/AreaMapping/AreaMap.jsx
@@ -15,14 +15,10 @@ const AreaMap = () => {
   const [areaId, setAreaId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(null);
-  // console.log(localStorage.getItem("assignedRider"), "assignedRider");
-  // const [assignedRider, setAssignedRider] = useState(
-  //   localStorage.getItem("assignedRider") !== undefined
-  //     ? JSON.parse(localStorage.getItem("assignedRider"))
-  //     : null
-  // ); // Initialize assignedRider state with data from localStorage
 
-  const [assignedRider, setAssignedRider] = useState(null);
+  const [assignedRider, setAssignedRider] = useState(
+    JSON.parse(localStorage.getItem("assignedRider")) || null
+  ); // Initialize assignedRider state with data from localStorage
 
   useEffect(() => {
     mappingList()

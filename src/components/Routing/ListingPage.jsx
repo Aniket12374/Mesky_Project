e4e00fdc9d5b7197ee9227a523454c373a964e59
manan-sharma01 <manan.sharma@mesky.com>
2024-05-02 14:12:05 +0530

    rank routing

--- src/components/Routing/ListingPage.jsx
+++ src/components/Routing/ListingPage.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { SearchOutlined } from "@ant-design/icons";
-import { Button, Input, Select, Space, Table } from "antd";
+import { Button, Form, Input, Select, Space, Table } from "antd";
 import Highlighter from "react-highlight-words";
-import { CSVLink } from "react-csv";
+// import { CSVLink } from "react-csv";
 import { routingStats } from "../../services/routing/RoutingService";
 import { rankInfo } from "../../services/routing/RoutingService";
 import { mappingList } from "../../services/areaMapping/MappingService";
@@ -16,13 +16,104 @@ const ListingPage = () => {
   const [isEditMode, setIsEditMode] = useState(false);
   const [buttonText, setButtonText] = useState("Edit");
   const [riderID, setRiderID] = useState();
+  const EditableContext = React.createContext(null);
+
+  const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...props} />
+        </EditableContext.Provider>
+      </Form>
+    );
+  };
+
+  const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) => {
+    const [editing, setEditing] = useState(isEditMode);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    // useEffect(() => {
+    //   if (editing) {
+    //     inputRef.current?.focus();
+    //   }
+    // }, [editing]);
+
+    const toggleEdit = () => {
+      // setEditing(!editing);
+      form.setFieldsValue({
+        [dataIndex]: record[dataIndex],
+      });
+    };
+
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+
+        toggleEdit();
+        handleSave({
+          ...record,
+          ...values,
+        });
+      } catch (errInfo) {
+        console.log("Save failed:", errInfo);
+      }
+    };
+    let childNode = children;
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{
+            margin: 0,
+          }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input
+            ref={inputRef}
+            onPressEnter={save}
+            onBlur={save}
+            defaultValue={record?.rank}
+          />
+        </Form.Item>
+      ) : (
+        <div
+          className="editable-cell-value-wrap"
+          style={{
+            paddingRight: 24,
+          }}
+          onClick={toggleEdit}
+        >
+          {children}
+        </div>
+      );
+    }
+    return <td {...restProps}>{childNode}</td>;
+  };
 
   const mergeSame = (arr) => {
     let mapData = new Map();
     arr.map((x) => {
-      if (mapData.has(x.sector)) {
-        mapData.set(x.sector, [...mapData.get(x.sector), x]);
-      } else mapData.set(x.sector, [x]);
+      let newData = {
+        ...x,
+        rank: x.rank == "" ? 0 : x.rank,
+      };
+      if (mapData.has(x.sector))
+        mapData.set(x.sector, [...mapData.get(x.sector), newData]);
+      else mapData.set(x.sector, [newData]);
     });
 
     return Array.from(mapData.values()).flat();
@@ -71,6 +162,7 @@ const ListingPage = () => {
   }, []);
 
   const onChange = async (value) => {
+    setRiderID(value);
     const response = await routingStats(value);
     const finalData = mergeSame(response.data);
     setTableData(finalData);
@@ -169,12 +261,13 @@ const ListingPage = () => {
       ),
   });
 
-  const columns = [
+  const routingColumns = [
     {
       title: "Sector",
       dataIndex: "sector",
       key: "sector",
       width: "33%",
+
       ...getColumnSearchProps("sector"),
     },
     {
@@ -189,42 +282,85 @@ const ListingPage = () => {
       dataIndex: "rank",
       key: "rank",
       width: "33%",
+      editable: true,
+      render: (rank) => {
+        return rank == "" ? 0 : rank;
+      },
       ...getColumnSearchProps("rank"),
-      render: (text, record) =>
-        isEditMode ? (
-          <Input
-            value={text}
-            onChange={(e) => {
-              // Update the rank in the record
-              const updatedRecord = { ...record, rank: e.target.value };
-              // Update the table data with the updated record
-              const updatedData = tableData.map((item) =>
-                item.key === record.key ? updatedRecord : item
-              );
-              setTableData(updatedData);
-            }}
-          />
-        ) : (
-          text
-        ),
+      // render: (text, record) =>
+      //   isEditMode ? (
+      //     <Input
+      //       value={text}
+      //       onChange={(e) => {
+      //         // Update the rank in the record
+      //         const updatedRecord = { ...record, rank: e.target.value };
+      //         // Update the table data with the updated record
+      //         const updatedData = tableData.map((item) =>
+      //           item.key === record.key ? updatedRecord : item
+      //         );
+      //         setTableData(updatedData);
+      //       }}
+      //     />
+      //   ) : (
+      //     text
+      //   ),
     },
   ];
 
   // Duplicate columns for the second set
-  const columnsSecondSet = columns.map((col) => ({
+  const columnsSecondSet = routingColumns.map((col) => ({
     ...col,
     key: col.key + "2",
   }));
 
+  const handleSave = (row) => {
+    const newData = [...tableData];
+    const index = newData.findIndex((item) => row.id === item.id);
+
+    // const item = newData[index];
+    newData[index]["rank"] = row["rank"];
+
+    // console.log({ row });
+    // let final = [...newData, item];
+    // newData.splice(index, 1, {
+    //   ...item,
+    //   ...row,
+    // });
+
+    setTableData(newData);
+  };
+
   // Split tableData into two subsets
-  const tableDataFirstSet = tableData.slice(0, 10);
-  const tableDataSecondSet = tableData.slice(10);
+  const tableDataFirstSet = tableData.slice(0, 13);
+  const tableDataSecondSet = tableData.slice(13);
 
   const selectStyle = {
     width: "100%",
     fontWeight: "bold",
     color: "black",
   };
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+  const columns = routingColumns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
 
   return (
     <div>
@@ -272,14 +408,18 @@ const ListingPage = () => {
           <Table
             columns={columns}
             dataSource={tableDataFirstSet}
+            components={components}
+            rowClassName={() => "editable-row"}
             pagination={false}
           />
         </div>
         <div className="w-1/2">
           <Table
-            columns={columnsSecondSet}
+            columns={columns}
             dataSource={tableDataSecondSet}
             pagination={false}
+            components={components}
+            rowClassName={() => "editable-row"}
           />
         </div>
       </div>

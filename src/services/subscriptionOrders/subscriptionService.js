82104f7e4b4e00c6bd4044cf8278a4fedf1dc7e4
aneela <aneela.chalamcharla@mesky.com>
2024-05-10 13:41:12 +0530

    reassign agents

--- src/services/subscriptionOrders/subscriptionService.js
+++ src/services/subscriptionOrders/subscriptionService.js
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 import { httpVendor } from "../api-client";
 
 export const presentOrders = (page = 1, size = 10) => {
@@ -18,4 +19,16 @@ export const previousOrders = (page = 1, size = 10) => {
 
 export const subscriptionPause = (data) => {
   return httpVendor.post("/api/delivery/portal/pause_item", data);
+};
+
+export const reAssignAgent = () => {
+  return httpVendor
+    .get("/api/delivery/portal/reassign_agent")
+    .then((res) => {
+      console.log({ res });
+    })
+    .catch((err) => {
+      console.log({ err });
+      toast.error("Not working properly!");
+    });
 };

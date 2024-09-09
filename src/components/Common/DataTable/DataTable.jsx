cfe7c3a52e883d854f7e06ba104f2288414df231
balamurali55 <balamuralismart5@gmail.com>
2024-09-09 10:48:25 +0530

    customer/credit

--- src/components/Common/DataTable/DataTable.jsx
+++ src/components/Common/DataTable/DataTable.jsx
@@ -28,7 +28,7 @@ export const DataTable = ({
   const navigate = useNavigate();
   const location = useLocation();
 
-  const paths = ["/subscription", "/history", "/customer"];
+  const paths = ["/subscription", "/history", "/customer/credit"];
   const pathIncludes = paths.some((path) => location?.pathname.includes(path));
   const iscustomerPage = location?.pathname.includes("/customer");
 

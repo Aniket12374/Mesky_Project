8d989584c8026c2f30b539215469a0045f3b18f4
AneelaMesky <aneela.chalamcharla@mesky.com>
2024-09-21 18:50:34 +0530

    version 1 maps

--- src/components/Customer/AddressMap.jsx
+++ src/components/Customer/AddressMap.jsx
@@ -1,24 +1,71 @@
 import React, { useEffect, useState } from "react";
-import debounce from "lodash/debounce";
 import AddressForm from "./AddressForm";
+import { getPinDetails } from "../../services/customerInfo/CustomerInfoService";
 
 function Address({ url, data, showNext, setShowNext }) {
+  var map;
+  var infowindow;
+  var marker;
+
   const [address, setAddress] = useState({
-    line_1: "the sprin",
+    line_2: data?.line_2,
+    pincode: data?.pincode,
+    sector_name: data?.sector_name,
+    latitude: data?.latitude,
+    longitude: data?.longitude,
   });
+
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  // const [map, setMap] = useState(null);
 
   const handleChange = (val) => {
     setAddress((prev) => ({
       ...prev,
-      line_1: val,
+      line_2: val,
     }));
+
+    const input = document.getElementById("line_2");
+    const autocomplete = new google.maps.places.Autocomplete(input, {
+      componentRestrictions: { country: "IN" },
+    });
+
+    autocomplete.addListener("place_changed", function () {
+      const place = autocomplete.getPlace();
+      const latLngs = place?.geometry?.location;
+      const lat = latLngs?.lat();
+      const lng = latLngs?.lng();
+
+      let pincodeCompo = place?.address_components?.find((x) =>
+        x.types.includes("postal_code")
+      );
+      let pincode = pincodeCompo?.long_name;
+
+      getPinDetails(pincode).then((res) => {
+        const data = res?.data;
+        Object.keys(data).map((x) => {
+          onChange(x, data[x]);
+        });
+      });
+
+      setAddress((prev) => ({
+        ...prev,
+        line_2: place?.formatted_address,
+        sector_name: extractSectorValue(place?.formatted_address),
+        pincode,
+        longitude: lng,
+        latitude: lat,
+      }));
+      console.log({ map });
+      createMarker({ lat, lng }, map, true);
+    });
   };
 
-  // console.log({ address });
-  let map;
-  let infowindow;
-  let service;
-  let markers = [];
+  const onChange = (key, val) => {
+    setAddress((prev) => ({
+      ...prev,
+      [key]: val,
+    }));
+  };
 
   useEffect(() => {
     const head = document.querySelector("head");
@@ -26,105 +73,89 @@ function Address({ url, data, showNext, setShowNext }) {
 
     script.setAttribute("src", url);
     head.appendChild(script);
+    setScriptLoaded(true);
 
     return () => {
       head.removeChild(script);
     };
-  }, [url]);
+  }, []);
 
   useEffect(() => {
-    // address?.line_1.length > 5 && initMap();
-    getAutoSearch(address?.line_1);
-  }, [address]);
+    if (!map) initMap();
+    console.log("map called ");
+  }, [data, scriptLoaded]);
+
+  function createMarker(place, map) {
+    map = new google.maps.Map(document.getElementById("mapdiv"), {
+      center: { lat: place.lat, lng: place.lng },
+      zoom: 20,
+    });
+    marker = new google.maps.Marker({
+      position: { lat: place.lat, lng: place.lng },
+      map: map,
+    });
+
+    map.panTo(place);
+  }
 
   async function initMap() {
-    // const { Map } = await google.maps.importLibrary("maps");
-    const gurgaon = new google.maps.LatLng(28.457523, 77.026344);
+    let defaultLatLng = {
+      lat: Number(data?.latitude) || 28.457523,
+      lng: Number(data?.longitude) || 77.026344,
+    };
+
+    const gurgaon = new google.maps.LatLng(
+      defaultLatLng.lat,
+      defaultLatLng.lng
+    );
     map = new google.maps.Map(document.getElementById("mapdiv"), {
       center: gurgaon,
-      zoom: 15,
-      // mapId: "DEMO_MAP_ID",
+      zoom: 20,
     });
 
     infowindow = new google.maps.InfoWindow();
 
-    placeSearch(address?.line_1);
-
     map.addListener("click", (e) => {
-      const placeLatlng = e.latLng;
-      console.log({ placeLatlng, e, markers }, "initfn");
-      markers.length > 0 ? markers[0].setMap(null) : null;
-      placeSearch(placeLatlng);
-      // createMarker(placeLatlng, map, true);
-    });
+      const { placeId, latLng } = e;
+      var geocoder = new google.maps.Geocoder();
+      console.log("clicked map");
 
-    map.infowindow.open(map);
-  }
-
-  // console.log({ map, markers });
-
-  function createMarker(place, map, latlng = false) {
-    // const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    if (!latlng && (!place.geometry || !place.geometry.location)) return;
-
-    const marker = new google.maps.Marker({
-      map,
-      position: latlng ? place : place.geometry.location,
-    });
-    markers.push(marker);
-
-    // new google.maps.marker.AdvancedMarkerElement({
-    //   position: latlng ? place : place.geometry.location,
-    //   map: map,
-    // });
-
-    google.maps.event.addListener(marker, "click", () => {
-      // infowindow.setContent(place.name || "");
-      console.log({ place });
-      // infowindow.open(map);
-    });
-    map.panTo(latlng ? place : place.geometry.location);
-  }
-
-  const placeSearch = (line1) => {
-    const request = {
-      query: line1,
-      fields: ["name", "geometry"],
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i], map);
+      geocoder.geocode(
+        {
+          latLng,
+        },
+        function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+              console.log({ results });
+              onChange("line_2", results[0].formatted_address);
+            }
+          }
         }
-
-        map.setCenter(results[0].geometry.location);
+      );
+      const lat = latLng.lat();
+      const lng = latLng.lng();
+      // fetch the value of the e and set the value of search input -- done
+      if (marker && marker.setMap) {
+        marker.setMap(null);
       }
+      marker = new google.maps.Marker({
+        position: { lat, lng },
+        map: map,
+      });
     });
-  };
 
-  const getAutoSearch = async (text) => {
-    console.log({ text });
-    const { Map } = await google.maps.importLibrary("maps");
-    const options = {
-      // bounds: defaultBounds,
-      componentRestrictions: { country: "in" },
-      fields: ["address_components", "geometry", "icon", "name"],
-      strictBounds: false,
-    };
-    const gurgaon = new google.maps.LatLng(28.457523, 77.026344);
-    map = new google.maps.Map(document.getElementById("mapdiv"), {
-      center: gurgaon,
-      zoom: 15,
-      // mapId: "DEMO_MAP_ID",
-    });
-    const autocomplete = new google.maps.places.AutocompleteService(map);
-    autocomplete.getQueryPredictions({ input: address?.line_1 });
-    console.log(autocomplete, "place autocomplete");
-    const place = autocomplete.getPlaces();
+    infowindow.open(map);
+  }
 
-    console.log(place, autocomplete, "place autocomplete");
+  // createMarker({ lat: data?.latitude, lng: data?.longitude }, map);
+
+  const extractSectorValue = (line) => {
+    if (typeof line !== "string") return "";
+
+    const sectorPattern = /sector\s*[^,]*/i; // Match 'sector' followed by any number of spaces and then any characters except a comma
+    const result = line.match(sectorPattern);
+    return result ? result[0].trim() : "";
   };
 
   window.initMap = initMap;
@@ -132,21 +163,30 @@ function Address({ url, data, showNext, setShowNext }) {
   return (
     <>
       {!showNext ? (
-        <div className='' style={{ width: "100%" }}>
-          <div id='mapdiv' style={{ position: "static" }}></div>
-          <div>Your Current Location</div>
+        <div className='' style={{ width: "500px" }}>
+          <div
+            id='mapdiv'
+            style={{ position: "static", width: "500px", height: "35vh" }}
+          ></div>
+          <div className='roboto-500 mt-3'>Your Current Address</div>
           <input
             type='text'
+            id='line_2'
             onChange={(e) => handleChange(e.target.value)}
-            value={address?.line_1}
-            className='border-2 border-gray-200'
+            value={address?.line_2}
+            className='border-2 border-gray-200 w-full focus:outline-none'
           />
           <button onClick={() => setShowNext(true)} className='search-btn'>
             Next
           </button>
         </div>
       ) : (
-        <AddressForm data={data} />
+        <AddressForm
+          data={{
+            ...data,
+            ...address,
+          }}
+        />
       )}
     </>
   );
